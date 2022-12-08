1e84b4bc8b04c248f1affb91c5a088a24ed27317
Jhon <59811681+okayda@users.noreply.github.com>
2022-12-08 13:16:20 -0800

    Add overlay with functionality

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@
 
 document.querySelector(".header-button").addEventListener("click", function () {
   document.querySelector("header").classList.add("active-header-nav");
+  document.querySelector(".overlay").style.display = "block";
 });
 
 document
   .querySelector(".main-nav-button")
   .addEventListener("click", function () {
     document.querySelector("header").classList.remove("active-header-nav");
+    document.querySelector(".overlay").style.display = "none";
   });
+
+document.querySelector(".overlay").addEventListener("click", function () {
+  document.querySelector("header").classList.remove("active-header-nav");
+  document.querySelector(".overlay").style.display = "none";
+});
